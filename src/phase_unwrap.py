f028e93a90128f4bb1155d5fced29896f694f339
Bryan Quah <bryanquah@gmail.com>
2024-06-14 14:20:58 -0700

    changes

--- src/phase_unwrap.py
+++ src/phase_unwrap.py
@@ -9,74 +9,45 @@ from preparation.preprocessing_pipeline import rescale_magnitude
 from tqdm import tqdm
 
 def main():
-    '''
-    Target folder should have Mag and Pha files
-    '''
     parser = create_parser()
     args = parser.parse_args()
 
-    files = [ os.path.join(args.path, f) for f in os.listdir(args.path) ]
+    # Rescale phase data
+    phase_nii = nib.load(args.phase)
+    phase_data = np.array(phase_nii.dataobj)
+    phase_data = rescale_magnitude(phase_data, -np.pi, np.pi)
+    phase_data = nib.Nifti1Image(phase_data, affine=phase_nii.affine, header=phase_nii.header)
+    phase_data.header.set_data_dtype('float32')
+    nib.save(phase_data, args.phase)
 
-    # Grab only the phase files
-    no_caipi_files = sorted([ f for f in files if 'segEPI_pha.nii.gz' in f ])
-    before_files = sorted([ f for f in files if '1x2_pha.nii.gz' in f or '1x3_pha.nii.gz' in f or '2x2_pha.nii.gz' in f ])
-    after_files = sorted([ f for f in files if 'Pha.nii.gz' in f ])
-    # Grab the magnitude brain extracted mask
-    bet_files = sorted([ f for f in files if 'bet' in f ])
+    outpath = '/'.join(args.phase.split('/')[:-1])
+    outname = args.phase.split('/')[-1].split('.')[0] + '_unwrap' # /dir/path/file.nii.gz -> file_unwrap
 
-    assert len(before_files) == len(after_files)
+    matlab_fn = f"Gen_tissue_phase('{args.bet}', '{args.phase}', '{outpath}', '{outname}')"
+    print(matlab_fn)
 
+    # Run phase unwrapping
     SCRIPT_DIR = '/home/quahb/caipi_denoising/STISuite_V3.0/'
-    strip_path_fext = lambda x: x.split('/')[-1].split('.')[0]
+    result = subprocess.run(
+            ['matlab', '-batch', matlab_fn],
+            cwd=SCRIPT_DIR,
+            capture_output=True,
+            text=True
+    )
+    #print(result.stdout)
+    print(result.stderr)
 
-    to_unwrap = [*no_caipi_files, *before_files, *after_files]
-    print(f'Processing phase files:\n{to_unwrap}\n')
-
-    pbar = tqdm(to_unwrap)
-    for phase_file in pbar:
-        pbar.set_description(f'Unwrapping {phase_file}')
-
-        # Grab corresponding brain extracted file
-        if 'CAIPI' not in phase_file:
-            bet_file = bet_files[0]
-        elif 'CAIPI' in phase_file:
-            if '1x2' in phase_file:
-                match = [b for b in bet_files if '1x2' in b]
-            elif '1x3' in phase_file:
-                match = [b for b in bet_files if '1x3' in b]
-            elif '2x2' in phase_file:
-                match = [b for b in bet_files if '2x2' in b]
-            bet_file = match[0]
-        else:
-            raise NotImplementedError()
-
-        # Rescale phase data
-        phase_nii = nib.load(phase_file)
-        phase_data = np.array(phase_nii.dataobj)
-        phase_data = rescale_magnitude(phase_data, -np.pi, np.pi)
-        phase_data = nib.Nifti1Image(phase_data, affine=phase_nii.affine, header=phase_nii.header)
-        phase_data.header.set_data_dtype('float32')
-        nib.save(phase_data, phase_file)
-
-        pha_subj = strip_path_fext(phase_file) + '_unwrap'
-        matlab_fn = f"Gen_tissue_phase('{bet_file}', '{phase_file}', '{args.path}', '{pha_subj}')"
-        print(matlab_fn)
-
-        # Run phase unwrapping
-        result = subprocess.run(
-                ['matlab', '-batch', matlab_fn],
-                cwd=SCRIPT_DIR,
-                capture_output=True,
-                text=True
-        )
-        #print(result.stdout)
-        print(result.stderr)
-        print(f'Completed {pha_subj}')
-
+    res = os.path.join(outpath, outname + '.nii.gz')
+    out_nii = nib.load(res)
+    out_data = np.array(out_nii.dataobj)
+    out_data = nib.Nifti1Image(out_data, affine=phase_nii.affine, header=phase_nii.header)
+    out_data.header.set_data_dtype('float32')
+    nib.save(out_data, res)
 
 def create_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('path')
+    parser.add_argument('bet')
+    parser.add_argument('phase')
 
     return parser
 
--- src/run_fold_predictions.sh
+++ src/run_fold_predictions.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 
 script="predict_torch.py"
-experiment="magnitude_2d_full"
+experiment="compleximage_2d_full"
 dims=2
-model_type="dncnn"
-input_size="384 384"
-path="/home/quahb/caipi_denoising/data/datasets/accelerated/${experiment}/{inputs,torch_f1}"
-#patching="--extract_patches --extract_step 32 32 32"
+model_type="cdncnn"
+path="/home/quahb/caipi_denoising/data/datasets/accelerated/${experiment}/{inputs,outputs}"
+
+# Choice A: 2D 
+input_size="--input_size 384 384"
 patching=""
 
+# Choice B: 3D
+#input_size="--input_size 64 64 64"
+#patching="--extract_patches --extract_step 32 32 32"
+
 models=(
-"/home/quahb/caipi_denoising/models/${experiment}_fold1_2024-01-09/${model_type}_ep20.pt"
-"/home/quahb/caipi_denoising/models/${experiment}_fold2_2024-01-09/${model_type}_ep16.pt"
-"/home/quahb/caipi_denoising/models/${experiment}_fold3_2024-01-09/${model_type}_ep16.pt"
-"/home/quahb/caipi_denoising/models/${experiment}_fold4_2024-01-09/${model_type}_ep9.pt"
-"/home/quahb/caipi_denoising/models/${experiment}_fold5_2024-01-10/${model_type}_ep5.pt"
+"/home/quahb/caipi_denoising/models/${experiment}_fold1_2024-02-28/${model_type}_ep100.pt"
+"/home/quahb/caipi_denoising/models/${experiment}_fold2_2024-02-29/${model_type}_ep100.pt"
+"/home/quahb/caipi_denoising/models/${experiment}_fold3_2024-03-01/${model_type}_ep100.pt"
+"/home/quahb/caipi_denoising/models/${experiment}_fold4_2024-03-02/${model_type}_ep100.pt"
+"/home/quahb/caipi_denoising/models/${experiment}_fold5_2024-03-03/${model_type}_ep100.pt"
 )
 
 for idx in ${!models[@]}
 do
     fold=$((${idx} + 1))
     model=${models[$idx]}
-    cmd="python ${script} --type_dir --fold ${fold} ${dims} ${model_type} mse ${path} ${model} ${input_size} ${patching}"
+    cmd="python ${script} --type_dir --fold ${fold} ${dims} ${path} ${model} ${input_size} ${patching}"
     echo ${cmd}
     echo ""
     eval ${cmd}
